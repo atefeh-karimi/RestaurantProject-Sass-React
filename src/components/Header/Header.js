bcc55bbe22ee90febefa892443f3d85066fa6165
atefeh-karimi <atefehkarimikia0922@gmail.com>
2022-10-05 18:56:28 +0330

    Add search Box🔍

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,4 @@
+import { Box, InputBase, styled } from "@mui/material";
 import React from "react";
 import {
   FaUtensils,
@@ -7,7 +8,20 @@ import {
   FaInstagram,
 } from "react-icons/fa";
 import "./styles.modules.scss";
-
+const Search = styled(Box)(({ theme }) => ({
+  display: "flex",
+  justifyContent: "center",
+  padding: "7px 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "50%",
+  position: "relative",
+  top: "30px",
+  left: 0,
+  right: 0,
+  margin: "0 auto",
+  background: " rgb(133 121 121 / 18%)",
+  backdropFilter: "blur(20px)",
+}));
 function Header() {
   return (
     <header>
@@ -17,9 +31,18 @@ function Header() {
           <button className="signup">ثبت نام</button>
           <FaUtensils className="header__logo" />
         </div>
+
+        <Search bgcolor={"background.default"} color={"text.primary"}>
+          <InputBase
+            sx={{ width: "100%", fontSize: "18px" }}
+            placeholder="جستجو..."
+          />
+        </Search>
+
         <a href="#gallery" className="header__iconDown">
           <FaAngleDoubleDown />
         </a>
+
         <div className="header__social">
           <span>
             <FaPinterest />
