cee99f556f1966910c4af3283cd27cf84d78ba63
atefeh-karimi <atefehkarimikia0922@gmail.com>
2022-10-10 12:39:01 +0330

    fixed url

--- src/components/AppPromotion.js
+++ src/components/AppPromotion.js
@@ -4,8 +4,8 @@ function AppPromotion() {
   return (
     <>
       <img src="./image/android-google-play.svg" alt="google-play" />
-      <img src="/image/apple-app-sotre.svg" alt="app-sotre" />
-      <img src="/image/huawei-download-icon.png" alt="huawei-download" />
+      <img src="./image/apple-app-sotre.svg" alt="app-sotre" />
+      <img src="./image/huawei-download-icon.png" alt="huawei-download" />
     </>
   );
 }
